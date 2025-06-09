7e71455ec1c400716757c33e1015eaddb72f4b74
Nyla Dickerson <nylaj726@gmail.com>
2025-06-09 12:42:02 -0400

    Add files via upload

---
+++ Lab2Task3.java
@@ -0,0 +1,11 @@
+//Author:Nyla Dickerson
+//Purpose:
+public class Lab2Task3 {
+  public static void main(String [] args){
+    int num1 =62;
+    int num2 =99;
+    int total =num1 + num2;
+    System.out.println("The total is:" + total);
+  }
+}
+     
